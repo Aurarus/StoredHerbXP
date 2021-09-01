2ebfd5afb6e155edcd207b40da59e34ff529cce4
Aurarus <aurarusdan@gmail.com>
2021-09-01 18:10:06 +0100

    First commit

--- src/main/java/com/storedherbxp/StoredHerbXPPlugin.java
+++ src/main/java/com/storedherbxp/StoredHerbXPPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.storedherbxp;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Stored Herb XP"
 )
-public class ExamplePlugin extends Plugin
+public class StoredHerbXPPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private StoredHerbXPConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	StoredHerbXPConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(StoredHerbXPConfig.class);
 	}
 }
--- src/test/java/com/storedherbxp/StoredHerbXPTest.java
+++ src/test/java/com/storedherbxp/StoredHerbXPTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.storedherbxp;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class StoredHerbXPTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(StoredHerbXPPlugin.class);
 		RuneLite.main(args);
 	}
 }
